499b227ad1288bb291b6b9638dd00f1bea9be677
swathisarraju <97828446+swathisarraju@users.noreply.github.com>
2023-06-13 22:19:49 +0530

    name,imgurl and desc added to UC3

--- usercard3.js
+++ usercard3.js
@@ -8,8 +8,12 @@ let displayobject;
 let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json()) //json is JS obj to print data
-    .then(data => console.log(data)) 
-    document.getElementById("MSD-img").src=displayobject.imgurl;
-    document.getElementById("MSD-name").innerHTML=displayobject.name;
-    document.getElementById("MSD-desc").innerHTML=displayobject.description;
+    .then(data=>{
+        displayobject.name=data.results[0].name.first +" "+ data.results[0].name.last
+        displayobject.imgurl= data.results[0].picture.large
+        displayobject.description=data.results[0].gender
+        document.getElementById("MSD-img").src=displayobject.imgurl;
+        document.getElementById("MSD-name").innerHTML=displayobject.name;
+        document.getElementById("MSD-desc").innerHTML=displayobject.description;
+    } 
 }
