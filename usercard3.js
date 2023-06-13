fb99850d7b3c9ddb9d2912f48dc3800a1797baea
swathisarraju <97828446+swathisarraju@users.noreply.github.com>
2023-06-13 22:00:03 +0530

    json obj

--- usercard3.js
+++ usercard3.js
@@ -8,7 +8,7 @@ let displayobject;
 let getRandomUser = function(){
     fetch("https://randomuser.me/api")
     .then(response => response.json()) //json is JS obj to print data
-    .then(data => console.log(data))
+    .then(data => console.log(data)) 
     document.getElementById("MSD-img").src=displayobject.imgurl;
     document.getElementById("MSD-name").innerHTML=displayobject.name;
     document.getElementById("MSD-desc").innerHTML=displayobject.description;
