5531002e1abf27fa408da8152bd30e17efd938c6
swathisarraju <97828446+swathisarraju@users.noreply.github.com>
2023-06-13 21:55:12 +0530

    json obj

--- usercard3.js
+++ usercard3.js
@@ -4,23 +4,11 @@ let MSDhonioldinfo={
     description:"Cricketer"
 } 
 
-let MSDyounginfo={
-    imgurl:"https://static.wixstatic.com/media/a3ab99_84fd4beccd76400984e1bbbbbc0d9a89~mv2.jpg/v1/fill/w_640,h_428,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/a3ab99_84fd4beccd76400984e1bbbbbc0d9a89~mv2.jpg",
-    name: "Mahendra Singh Dhoni",
-    description:"Captian of Indian Team"
-}
-
-let isMSDold=true;
 let displayobject;
-let flipdata = function(){
-    if (isMSDold==true){
-        displayobject=MSDyounginfo;
-        isMSDold=false;
-    }
-    else{
-        displayobject=MSDhonioldinfo;
-        isMSDold=true;
-    }
+let getRandomUser = function(){
+    fetch("https://randomuser.me/api")
+    .then(response => response.json()) //json is JS obj to print data
+    .then(data => console.log(data))
     document.getElementById("MSD-img").src=displayobject.imgurl;
     document.getElementById("MSD-name").innerHTML=displayobject.name;
     document.getElementById("MSD-desc").innerHTML=displayobject.description;
